2cfd5075eb913e3a90f985a1a4d68231ede9a6cf
ohiduzzamansiam1 <ohiduzzamansiam1@gmail.com>
2023-12-29 16:14:36 +0600

    adding speed insigths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import CartProvider from "./Providers";
@@ -26,6 +27,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Analytics />
+          <SpeedInsights />
         </CartProvider>
       </body>
     </html>
